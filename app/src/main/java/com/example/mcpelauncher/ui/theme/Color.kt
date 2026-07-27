d3ae4aec961bf4b85c09a845e3322a7636a1f023
nashyy-ig <nashsazuki@gmail.com>
2026-07-27 11:56:40 +0530

    Create Color.kt

---
+++ app/src/main/java/com/example/mcpelauncher/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.mcpelauncher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GrassGreen = Color(0xFF4C9A2A)
+val DirtBrown = Color(0xFF6B4423)
+val StoneGray = Color(0xFF7A7A7A)
+val DeepSlate = Color(0xFF1E1E1E)
+val Obsidian = Color(0xFF120E1A)
+val GoldOre = Color(0xFFF2C94C)
+val DiamondCyan = Color(0xFF5CE1E6)
+val OffWhite = Color(0xFFEDEDED)
